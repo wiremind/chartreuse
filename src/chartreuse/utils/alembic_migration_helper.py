8241a47e78a889a5537f8a99a9d42cec7dd2a6bc
Cédric de Saint Martin <cdesaintmartin@wiremind.fr>
2019-01-25 12:27:53 +0100

    v0.8.5: Do not assume it is okay if could not authenticate to postgresql.

--- src/chartreuse/utils/alembic_migration_helper.py
+++ src/chartreuse/utils/alembic_migration_helper.py
@@ -71,11 +71,15 @@ class AlembicMigrationHelper(object):
         os.chdir("/app/alembic")
         try:
             sqlalchemy.create_engine(self.database_url).table_names()
-        except sqlalchemy.exc.OperationalError:
-            print(
-                "Could not reach postgresql, assuming postgres server does not exist yet or is down."
-            )
-            return False
+        except sqlalchemy.exc.OperationalError as e:
+            if 'Connection refused' in e.orig.args[0]:
+                print(
+                    "Could not reach postgresql, assuming postgres server does not exist yet or is down."
+                )
+                print(e.orig.args[0])
+                return False
+            # Raise any other error instead of swallowing it.
+            raise e
         return True
 
     def is_postgres_empty(self):
